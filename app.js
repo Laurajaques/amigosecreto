e8ac70dbd6673d0e5cb3565c9bbff6c2f2b6eb09
Laurajaques <llaurajaques@gmail.com>
2025-09-22 20:21:58 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, digite um nome válido!");
+        return;
+    }
+
+    amigos.push(nome);
+
+    atualizarLista();
+
+    input.value = "";
+}
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo antes de sortear!");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 O amigo secreto sorteado é: <strong>${amigoSorteado}</strong></li>`;
+}
